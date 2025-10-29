638d2a0d6acdd7796134eb7c4520337124f16fe5
tfelix <tfelix205@gmail.com>
2025-10-29 12:18:39 +0100

    bug fix v8

--- models/groupMembers.js
+++ models/groupMembers.js
@@ -1,40 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-    const Membership = sequelize.define('Membership', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true
-        },
-        userId: {
-            type: DataTypes.UUID,
-            allowNull: false
-        },
-        groupId: {
-            type: DataTypes.UUID,
-            allowNull: false,
-        },
-        role: {
-            type: DataTypes.STRING,
-            defaultValue: 'member',
-        },
-        membershipStatus: {
-            type: DataTypes.ENUM('pending', 'active', 'rejected'),
-            defaultValue: 'pending',
-        },
-        paymentStatus: {
-            type: DataTypes.ENUM('paid', 'unpaid'),
-            defaultValue: 'unpaid',
-        },
-        hasReceived: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-            allowNull: false
-        },
+  const Membership = sequelize.define('Membership', {
+    id: { 
+      type: DataTypes.UUID, 
+      defaultValue: DataTypes.UUIDV4, 
+      primaryKey: true 
+    },
+    userId: { 
+      type: DataTypes.UUID, 
+      allowNull: false 
+    },
+    groupId: { 
+      type: DataTypes.UUID, 
+      allowNull: false 
+    },
+    role: { 
+      type: DataTypes.STRING, 
+      defaultValue: 'member' 
+    },
+    
+    status: {
+      type: DataTypes.ENUM('pending', 'active', 'rejected'),
+      defaultValue: 'pending'
+    },
 
-      },  {
-            tableName: 'group_members',
-            timestamps: true,
-        });
+  }, {
+    tableName: 'memberships',
+    timestamps: true,
+  });
 
-    return Membership
+  return Membership;
 };
