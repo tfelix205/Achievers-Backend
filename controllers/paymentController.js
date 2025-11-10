7f0d3b7ddbabe948faf8f4cac9d3927ef75d851a
tfelix <tfelix205@gmail.com>
2025-11-10 12:14:10 +0100

    verify v2

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -30,7 +30,7 @@ exports.initializeContribution = async (req, res) => {
       amount: amount,
       customerName: user.name,
       customerEmail: user.email,
-      redirectUrl: `${process.env.FRONTEND_URL}#/contribution/verify/${reference}`,
+      redirectUrl: `${process.env.FRONTEND_URL}#/contribution/verify/`,
       reference: reference,
       metadata: {
         userId: userId,
